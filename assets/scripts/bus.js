3e86489ac6934f7cb3680335f8204811c10fbf21
Julius Tens <mail@juliustens.eu>
2018-05-08 00:09:47 +0200

    horsey@4

--- assets/scripts/bus.js
+++ assets/scripts/bus.js
@@ -1,38 +1,10 @@
 'use strict'
 
+const addAutocomplete = require('./common')
+
 const api = {
 	url: 'https://1.mfb.juliustens.eu/regions',
-	adapter: (res) => res.map((e) => e.name)
+	adapter: res => res.map(e => e.name)
 }
-const autocomplete = require('horsey')
-const fetch = require('fetch-ponyfill')().fetch
-const querystring = require('querystring').stringify
 
-autocomplete(document.querySelector('#originInput'), {
-	suggestions: (value, done) => {
-		fetch(api.url+'?'+querystring({query: value}), {
-			method: "get",
-			mode: "cors"
-		}).then((r) => r.json()).then(api.adapter).then((res) => done(res))
-	},
-	limit: 5,
-	appendTo: document.querySelector('#origin'),
-	autoHideOnClick: true,
-	autoHideOnBlur: true
-})
-
-autocomplete(document.querySelector('#destinationInput'), {
-	suggestions: (value, done) => {
-		fetch(api.url+'?'+querystring({query: value}), {
-			method: "get",
-			mode: "cors"
-		}).then((r) => r.json()).then(api.adapter).then((res) => done(res))
-	},
-	limit: 5,
-	appendTo: document.querySelector('#destination'),
-	autoHideOnClick: true,
-	autoHideOnBlur: true
-})
-
-document.querySelector('#originInput').addEventListener('horsey-selected', () => document.querySelector('#destinationInput').focus())
-document.querySelector('#destinationInput').addEventListener('horsey-selected', () => document.querySelector('#submit').focus())
+addAutocomplete(api)
