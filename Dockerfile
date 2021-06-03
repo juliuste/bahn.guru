6092ae1aa7e361edff9ea16336b66dba150497b9
Julius Tens <mail@juliustens.eu>
2021-06-03 22:31:39 +0200

    add docker setup

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# install dependencies
+FROM node:fermium-alpine
+WORKDIR /app-src
+
+RUN npm i -g pnpm
+
+COPY assets ./assets
+
+COPY package.json ./
+RUN pnpm install
+
+COPY apis ./apis
+COPY lib ./lib
+COPY index.js ./index.js
+
+RUN pnpm run build
+
+USER node
+
+ENV PORT=3000
+ENV API="bahn"
+
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import createServer from './lib/index.js'
 import * as bahn from './apis/bahn/index.js'
 import * as bus from './apis/bus/index.js'
 
-const port = process.env.PORT || 3000
+const port = (+process.env.PORT) || 3000
 const apiId = process.env.API || 'bahn'
 
 const apis = { bahn, bus }
@@ -14,5 +14,5 @@ const server = createServer(api)
 // start HTTP server
 server.listen(port, error => {
 	if (error) return console.error(error)
-	console.log(`HTTP: Listening on ${port}.`)
+	console.log(`HTTP: Listening on port ${port}.`)
 })
