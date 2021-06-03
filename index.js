a3c026ed1002f1120c44dd343c83883bce54e768
Julius Tens <mail@juliustens.eu>
2021-06-03 21:57:35 +0200

    remove file logging

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import { createWriteStream } from 'fs'
-import { join, dirname } from 'path'
 import createServer from './lib/index.js'
 import * as bahn from './apis/bahn/index.js'
 import * as bus from './apis/bus/index.js'
-import { fileURLToPath } from 'url'
 
 const port = process.env.PORT || 3000
 const apiId = process.env.API || 'bahn'
@@ -12,12 +9,7 @@ const apis = { bahn, bus }
 const api = apis[apiId]
 if (!api) throw new Error('Unknown api selected.')
 
-const logging = !!process.env.LOGGING
-// create a write stream (in append mode) if logging is enabled
-let accessLogStream
-if (logging) accessLogStream = createWriteStream(join(dirname(fileURLToPath(import.meta.url)), `access-${apiId}.log`), { flags: 'a' })
-
-const server = createServer({ api, accessLogStream })
+const server = createServer(api)
 
 // start HTTP server
 server.listen(port, error => {
